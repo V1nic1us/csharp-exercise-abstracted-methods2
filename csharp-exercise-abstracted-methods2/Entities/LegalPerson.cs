b6b34195e1e31501d0b2e77ae9cd03eccef8cfe7
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-12 21:34:59 -0400

    feat: add legal person class

---
+++ csharp-exercise-abstracted-methods2/Entities/LegalPerson.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercise_abstracted_methods2.Entities
+{
+    internal class LegalPerson: Person
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public LegalPerson(string name, double annualIncome, int numberOfEmployees) : base(name, annualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            if (NumberOfEmployees > 10)
+            {
+                return AnnualIncome * 0.14;
+            }
+            else
+            {
+                return AnnualIncome * 0.16;
+            }
+        }
+    }
+}
